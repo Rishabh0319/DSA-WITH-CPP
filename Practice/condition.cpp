9c5dae774c1fbf12cb0062a9dd3f4d96deee4c28
Rishabh Tripathi <rishabhtripathi0319@gmail.com>
2022-08-25 10:20:15 +0530

    git ignore added

---
+++ Practice/condition.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    // Example 1
+
+    /* int saving;
+
+    cout<< "Enter your Savings: ";
+    cin>> saving; 
+   
+    if(saving > 5000)
+    {
+        cout<< "Rashmi";
+    }
+    else
+    {
+        cout<< "neha";
+    } */
+
+    int saving;
+
+    cout<< "Enter Your Saving";
+    cin>> saving;
+    
+    
+
+    return 0;
+}
---
+++ Practice/firstprogram.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string first_name, last_name; 
+    
+    cout<<"Enter Your First Name: ";
+    cin>>first_name;
+
+    cout<<"Enter your last name: ";
+    cin>>last_name;
+
+    cout<<"your full name is "<<first_name <<last_name;
+
+    return 0;
+}
